4a2267bd719a3d9aa19c749ff3070e099dd338f7
犯花 <15156833+offending-the_flower@user.noreply.gitee.com>
2026-03-04 16:47:14 +0800

    first commit

---
+++ Core/Src/main.c
@@ -0,0 +1,93 @@
+#include "main.h"
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
+
+void SystemClock_Config(void);
+
+char a[] = "Hello USB!";
+
+int main(void)
+{
+	HAL_Init();
+	SystemClock_Config();
+	
+	MX_USB_DEVICE_Init();																	// 初始化USB
+	
+	while (1)
+	{
+		USB_printf(">>>\r\n%s\r\n", a);
+		HAL_Delay(500);
+	}
+}
+
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_HSI48;   // 添加 HSI48
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;                                            // 使能 HSI48
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 2;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLP = 1;
+  RCC_OscInitStruct.PLL.PLLQ = 6;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  // 以下时钟配置不变
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+	  
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif
---
+++ Hardware/usb_device.c
@@ -0,0 +1,45 @@
+#include "usb_device.h"
+#include "usbd_core.h"
+#include "usbd_desc.h"
+#include "usbd_cdc.h"
+#include "usbd_cdc_if.h"
+
+USBD_HandleTypeDef hUsbDeviceHS;
+
+void MX_USB_DEVICE_Init(void)
+{
+	uint16_t	 	USB_TimeOut;														// 超时判断时间
+	uint32_t  		Tickstart;															// 计时起始时间
+	
+	if (USBD_Init(&hUsbDeviceHS, &FS_Desc, DEVICE_HS) != USBD_OK)						// USB初始化
+	{
+		Error_Handler();
+	}
+	if (USBD_RegisterClass(&hUsbDeviceHS, &USBD_CDC) != USBD_OK)
+	{
+		Error_Handler();
+	}
+	if (USBD_CDC_RegisterInterface(&hUsbDeviceHS, &USBD_Interface_fops_FS) != USBD_OK)
+	{
+		Error_Handler();
+	}
+	if (USBD_Start(&hUsbDeviceHS) != USBD_OK)
+	{
+		Error_Handler();
+	}
+
+	HAL_PWREx_EnableUSBVoltageDetector();
+
+ 	USB_TimeOut = 1000;																	// 这里取1000ms
+	Tickstart = HAL_GetTick();															// 获取systick当前时间
+	while(  hUsbDeviceHS.dev_state != USBD_STATE_CONFIGURED )							// 等待USB建立连接
+	{
+		if((HAL_GetTick() - Tickstart) > USB_TimeOut) 									// 判断是否超时
+		{
+			break;																		// 若超时则跳出
+		}
+	}
+
+	HAL_Delay(200);																		// 建立连接之后，延时等待一段时间，不然第一次发送的数据容易丢失		
+	 
+}
